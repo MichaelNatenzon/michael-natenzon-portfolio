a5c2b6dd7628f06bdf41da179a76788d6f7bd599
Michael Natenzon <natenzonm@gmail.com>
2022-07-06 21:49:00 -0400

    Google fonts display set to swap

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;700&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
