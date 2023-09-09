0a9b2bdd8be6e38c7df4439d524e7087d75393b3
Michael Natenzon <natenzonm@gmail.com>
2023-09-09 19:07:16 -0400

    Fixed revalidation

--- pages/index.js
+++ pages/index.js
@@ -33,6 +33,7 @@ export async function getStaticProps() {
     props: {
       pageContent,
     },
+    revalidate: 60,
   };
 }
 
--- pages/plot-generator.js
+++ pages/plot-generator.js
@@ -27,6 +27,7 @@ export async function getStaticProps() {
     props: {
       pageContent,
     },
+    revalidate: 60,
   };
 }
 
